effed408e97784a9c3ef929977153797053bb00f
mhmmdrifkikrnwn <mhmmdrifkikrnwn@gmail.com>
2018-07-28 01:55:41 +0800

    Update 2.js

--- xyz/2.js
+++ xyz/2.js
@@ -25,5 +25,5 @@ if(a_to_vk)
 {a_to_vk.innerHTML+=a_to_va;}}
 function a_to_fa()
 {var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
-protected_links = "xxx";
+protected_links = "mhmmdrifkikrnwn.blogspot.com";
 auto_safelink();
